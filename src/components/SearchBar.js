a9f836117a06c999e395dae56330dd40f5cf14c6
yanlifang <yanli@oregonstate.edu>
2020-06-06 09:45:44 -0500

    set up search box, API needed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,22 +6,21 @@ class SearchBar extends React.Component{
 
     onFormSubmit = event =>{
         event.preventDefault();
-
-        this.props.onFormSubmit(this.state.term);
+        this.props.onSubmit(this.state.term);
     }
-}
 
-
-render(){
-    return (
-        <div className="ui segment">
-            <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <input type="text" value={this.state.term} onChange={(e) =>this.ListeningStateChangedEvent({term: e.target.value})} />
-                </div>
-            </form>
-        </div>       
-    );
+    render(){
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Reopen Policy Search</label>
+                        <input type="text" value={this.state.term} onChange={(e) =>this.setState({term: e.target.value})} />
+                    </div>
+                </form>
+            </div>       
+        );
+    }
 }
 
 export default SearchBar;
