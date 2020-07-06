d3ee4781555d1707e746c3582e7c187f1e293409
yanlifang <yanli@oregonstate.edu>
2020-07-06 18:36:03 -0500

    update for webpage

---
+++ webapp/src/components/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Search';
+
+const SearchPage = () =>{
+    return (
+        <div>
+            <Search />
+        </div>
+
+
+    );
+};
+
+export default SearchPage;
