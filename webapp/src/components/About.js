5969d91973082a27ab4af099f3b8fce24fcc4b57
yanlifang <yanli@oregonstate.edu>
2020-07-11 22:42:54 -0500

    add signin

---
+++ webapp/src/components/About.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class About extends Component{
+    render(){
+        return (
+            <div>
+                <p style={{margin: '30px'}}>
+                        Human Coronavirus Type 
+                    </p>
+                    <p>
+                        Coronaviruses include a group of RNA viruses that can cause diseases in mammals and birds. 
+                    </p>
+                    <p>
+                        Coronaviruses are large, roughly spherical, particles with crown like spikes proteins on their surface. 
+                        There are four main subgroups of coronavirus known as alpha, beta, gamma and delta.
+                        Human coronaviruses including the novel coronavirus - CoV19 all are alphacoronavirus. 
+                    </p>           
+                    <p>
+                        With the widespread of CoV19 worldwide,all countries have their own travel policies during this pandemic. 
+                        Due to the vairation of spread in each country, all states or province of the countries may have different 
+                        travel polices. 
+                    </p>
+                    <p>
+                        To facilitate of travel policy search, we started this project to pull all travel policy together.
+                    </p>
+                    <p>
+                        Be safe!     
+                    </p>  
+            </div>
+        );
+    }
+};
+
+export default About;
